756bcff3828df60b39f6a50963048fb26b33c48d
Kim Wong <33343260+jhuang239@users.noreply.github.com>
2026-04-15 11:27:10 +0800

    Fix formatting of education display in Education.tsx

--- src/ui/experiences/Education.tsx
+++ src/ui/experiences/Education.tsx
@@ -18,7 +18,7 @@ const Education: React.FC<EducationProps> = ({
     return (
         <div className="flex flex-col gap-2 shadow-2xl p-4 bg-[var(--background)] xl:h-50 h-60 rounded-md">
             <span className="text-xl font-bold capitalize">
-                {degree}, in {major} at
+                {degree}, {major} at
                 <span className="italic text-[#2dd4bf]"> {school}</span>
             </span>
             <span className="text-[#2dd4bf] font-bold">
